537c91224e06450117ba92334ae9744afca76304
Livio <34880763+UI-Mario@users.noreply.github.com>
2019-05-06 21:05:02 +0800

    Update moravec.py

--- moravec/moravec.py
+++ moravec/moravec.py
@@ -2,7 +2,7 @@ import cv2
 import math
 import numpy as np
 
-def MoravecCorners(I, kSize, threshold):
+def MoravecCorners(I, kSize, threshold, i_np, j_np):
 	M = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
 	H = np.array(M)
 	rows = I.shape[0]
@@ -20,7 +20,7 @@ def MoravecCorners(I, kSize, threshold):
 				v[1] += pow(int(H[i+k, j]) - int(H[i+k+1, j]), 2)
 				v[2] += pow(int(H[i+k, j+k]) - int(H[i+k+1, j+k+1]), 2)
 				v[3] += pow(int(H[i+k, j-k]) - int(H[i+k+1, j-k-1]), 2)
-			value = max(v)
+			value = min(v)
 			if value > threshold:
 				n+=1
 				#圆的绘制
@@ -29,11 +29,21 @@ def MoravecCorners(I, kSize, threshold):
 				#第三个参数  半径
 				#第四个参数  颜色
 				#第五个参数  填充或线宽
-				cv2.circle(H,(i,j),3,(211,0,213),1)
-	return H
+				i_np[n]=j
+				j_np[n]=i
+				#cv2.circle(H,(i,j),3,(211,0,213),1)
+	for m in range(0,n):
+		cv2.circle(H,(i_np[m],j_np[m]),3,(211,0,213),1)
+	return H,i_np,j_np
 
-img = cv2.imread('cook.jpg', 1)
-print(img[0,0])
-res = MoravecCorners(img, 5, 100)
+img = cv2.imread('1.jpg', 1)
+
+i_np = [0]*100000
+j_np = [0]*100000
+i_1=i_np
+j_1=j_np
+
+res, i_1, j_1 = MoravecCorners(img, 5, 10000, i_np, j_np)
 cv2.imshow('temp', res)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
